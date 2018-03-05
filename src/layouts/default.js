895e9530612abe5dab4cce1c13351bc6d8bf5fc5
Noah Klayman <noahklayman@gmail.com>
2018-03-04 20:41:58 -0800

    reverted to js as ts is broken

---
+++ src/layouts/default.js
@@ -0,0 +1,13 @@
+import { openURL } from 'quasar'
+
+export default {
+    name: 'LayoutDefault',
+    data () {
+        return {
+            leftDrawerOpen: false
+        }
+    },
+    methods: {
+        openURL
+    }
+}
