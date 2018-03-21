609c74b4878d8624b72bcbdafb7875afa251a5de
Noah Klayman <noahklayman@gmail.com>
2018-03-21 13:21:58 -0700

    replaced os.homedir/tmpdir with proccess.env.tmpdir/appdata

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -1,5 +1,4 @@
 import downloadFile from '../downloader.js'
-import os from 'os'
 import fs from 'fs-extra'
 import DecompressZip from 'decompress-zip'
 export default {
@@ -45,50 +44,73 @@ export default {
                 status: 'Downloading'
             })
             let localFile =
-                os.tmpdir() +
+                window.process.env.TEMP +
                 '/' +
                 this.$store.state.versions[version].name +
                 '.zip'
-            // downloadFile({
-            //     remoteFile: this.$store.state.versions[this.selected].download,
-            //     localFile,
-            //     onProgress: (received, total) => {
-            //         if (this.$store.state.selected === version) {
-            //             this.progress = parseInt(
-            //                 (received * 100 / total).toFixed(1)
-            //             )
-            //         }
-            //     }
-            // }).then(() => {
-            let extractedPath =
-                os.homedir() + '/AppData/Local/Blender Launcher/'
-            var unzipper = new DecompressZip(localFile)
-            this.$store.commit('setStatus', {
-                target: version,
-                status: 'Installing'
-            })
-            // Add the error event listener
-            unzipper.on('error', function (err) {
-                console.log('Caught an error', err)
-            })
+            downloadFile({
+                remoteFile: this.$store.state.versions[this.selected].download,
+                localFile,
+                onProgress: (received, total) => {
+                    if (this.$store.state.selected === version) {
+                        this.progress = parseInt(
+                            (received * 100 / total).toFixed(1)
+                        )
+                    }
+                }
+            }).then(() => {
+                let extractedPath =
+                    window.process.env.LOCALAPPDATA + '/Blender Launcher/'
+                var unzipper = new DecompressZip(localFile)
+                this.$store.commit('setStatus', {
+                    target: version,
+                    status: 'Installing'
+                })
+                // Add the error event listener
+                unzipper.on('error', function (err) {
+                    console.log('Caught an error', err)
+                })
 
-            // Notify when everything is extracted
-            unzipper.on('extract', log => {
-                let oldPath =
-                    os.homedir() + '/AppData/Local/Blender Launcher/' + version
-                setTimeout(() => {
-                    if (fs.existsSync(oldPath)) {
-                        // Do something
-                        fs.rename(oldPath, oldPath + '-old', () => {
+                // Notify when everything is extracted
+                unzipper.on('extract', log => {
+                    let oldPath =
+                        window.process.env.LOCALAPPDATA +
+                        '/Blender Launcher/' +
+                        version
+                    setTimeout(() => {
+                        if (fs.existsSync(oldPath)) {
+                            // Do something
+                            fs.rename(oldPath, oldPath + '-old', () => {
+                                fs.rename(
+                                    log[0].folder
+                                        ? extractedPath +
+                                          log[0].folder.split('\\')[0]
+                                        : extractedPath +
+                                          log[0].deflated.split('\\')[0],
+                                    oldPath,
+                                    () => {
+                                        fs.removeSync(oldPath + '-old')
+                                        this.$store.commit('setStatus', {
+                                            target: version,
+                                            status: 'Updated'
+                                        })
+                                        this.$store.commit(
+                                            'setVersion',
+                                            version
+                                        )
+                                        this.progress = 0
+                                    }
+                                )
+                            })
+                        } else {
                             fs.rename(
-                                log[0].folder
+                                log[0].folder !== undefined
                                     ? extractedPath +
                                       log[0].folder.split('\\')[0]
                                     : extractedPath +
                                       log[0].deflated.split('\\')[0],
                                 oldPath,
                                 () => {
-                                    fs.removeSync(oldPath + '-old')
                                     this.$store.commit('setStatus', {
                                         target: version,
                                         status: 'Updated'
@@ -97,40 +119,23 @@ export default {
                                     this.progress = 0
                                 }
                             )
-                        })
-                    } else {
-                        fs.rename(
-                            log[0].folder !== undefined
-                                ? extractedPath + log[0].folder.split('\\')[0]
-                                : extractedPath +
-                                  log[0].deflated.split('\\')[0],
-                            oldPath,
-                            () => {
-                                this.$store.commit('setStatus', {
-                                    target: version,
-                                    status: 'Updated'
-                                })
-                                this.$store.commit('setVersion', version)
-                                this.progress = 0
-                            }
+                        }
+                    }, 500)
+                })
+
+                unzipper.on('progress', (fileIndex, fileCount) => {
+                    if (this.$store.state.selected === version) {
+                        this.progress = parseInt(
+                            (fileIndex * 100 / fileCount).toFixed(1)
                         )
                     }
-                }, 500)
-            })
+                })
 
-            unzipper.on('progress', (fileIndex, fileCount) => {
-                if (this.$store.state.selected === version) {
-                    this.progress = parseInt(
-                        (fileIndex * 100 / fileCount).toFixed(1)
-                    )
-                }
+                // Start extraction of the content
+                unzipper.extract({
+                    path: extractedPath
+                })
             })
-
-            // Start extraction of the content
-            unzipper.extract({
-                path: extractedPath
-            })
-            // })
         }
     },
     props: ['tags']
