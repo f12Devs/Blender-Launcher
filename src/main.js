6914f326ab0cb5e26887def89b0b8ebc1a119a75
Noah Klayman <noahklayman@gmail.com>
2018-05-14 15:29:36 -0700

    add vue-cli-plugin-quasar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,48 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+import "./styles/quasar.styl";
+import "quasar-extras/animate";
+import "quasar-extras/roboto-font";
+import "quasar-extras/material-icons";
+import Quasar, {
+  QBtn,
+  QLayout,
+  QLayoutHeader,
+  QLayoutDrawer,
+  QPage,
+  QPageContainer,
+  QToolbar,
+  QToolbarTitle,
+  QList,
+  QListHeader,
+  QItemSeparator,
+  QItem,
+  QItemSide,
+  QItemMain
+} from "quasar";
+
+Vue.use(Quasar, {
+  components: {
+    QBtn,
+    QLayout,
+    QLayoutHeader,
+    QLayoutDrawer,
+    QPage,
+    QPageContainer,
+    QToolbar,
+    QToolbarTitle,
+    QList,
+    QListHeader,
+    QItemSeparator,
+    QItem,
+    QItemSide,
+    QItemMain
+  },
+  directives: {},
+  plugins: {}
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  pluginOptions: {
+    quasar: {
+      theme: "mat"
+    }
+  }
 };
