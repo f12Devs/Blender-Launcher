3adac139eadea7d93a21a8a05a1ed00dee9dd1c3
Noah Klayman <noahklayman@gmail.com>
2018-06-09 10:32:54 -0700

    add typings to downloader and install @types/request

--- src/downloader.ts
+++ src/downloader.ts
@@ -1,7 +1,20 @@
 import request from 'request'
 import fs from 'fs'
-export default function downloadFile (configuration) {
-    return new Promise(function (resolve, reject) {
+interface Configuration {
+    remoteFile: string
+    localFile: string
+    onProgress: Function
+}
+interface Chunk {
+    length: number
+}
+interface Data {
+    headers: {
+        'content-length': string
+    }
+}
+export default function downloadFile(configuration: Configuration) {
+    return new Promise(function(resolve) {
         // Save variable to know progress
         var receivedBytes = 0
         var totalBytes = 0
@@ -14,27 +27,27 @@ export default function downloadFile (configuration) {
         var out = fs.createWriteStream(configuration.localFile)
         req.pipe(out)
 
-        req.on('response', function (data) {
+        req.on('response', function(data: Data) {
             // Change the total bytes value to get progress later.
             totalBytes = parseInt(data.headers['content-length'])
         })
 
         // Get progress if callback exists
         if (configuration.hasOwnProperty('onProgress')) {
-            req.on('data', function (chunk) {
+            req.on('data', function(chunk: Chunk) {
                 // Update the received bytes
                 receivedBytes += chunk.length
 
                 configuration.onProgress(receivedBytes, totalBytes)
             })
         } else {
-            req.on('data', function (chunk) {
+            req.on('data', function(chunk: Chunk) {
                 // Update the received bytes
                 receivedBytes += chunk.length
             })
         }
 
-        req.on('end', function () {
+        req.on('end', function() {
             resolve()
         })
     })
